aa48a7b9c5d8c6c5b28c8292ed84096b27a342bf
davidkenesaw <davidkennesaw@gmail.com>
2023-10-07 01:56:40 -0400

    register functionality done

--- Server/Auth.py
+++ Server/Auth.py
@@ -44,3 +44,70 @@ def login():
 
     #json object response
     return jsonify(data)
+
+@Auth.route("/RegisterOrg", methods=('POST',))
+def RegOrg():
+    #request input
+    #{
+    #   OrgName: 
+    #   UserName:
+    #   Password:
+    #   FirstName:
+    #   LastName:
+    #   Email:
+    #}
+    req = request.get_json()
+    OrgName = req['OrgName']
+    UserName = req['UserName']
+    Password = req['Password']
+    FirstName = req['FirstName']
+    LastName = req['LastName']
+    Email = req['Email']
+
+    cur = conn.cursor()
+    #query for finding user user in 
+    query_string = "SELECT * FROM Users WHERE UserName = %s"
+
+    #query execute
+    cur.execute(query_string,[UserName])
+    User = cur.fetchall()
+
+
+    cur1 = conn.cursor()
+    #query for finding user user in 
+    query_string = "SELECT * FROM Organizations WHERE OrgName = %s"
+
+    #query execute
+    cur1.execute(query_string,[OrgName])
+    Org = cur1.fetchall()
+
+
+    if(User or Org):#if user info is correct
+        data = {
+            "Registered": "False"
+        }
+    else:#if user info is not correct
+        
+        insertUser = conn.cursor()
+
+        sql = "INSERT INTO Users (UserName, Password, FirstName, LastName, Email, AccountType, Organization) VALUES (%s, %s,%s, %s,%s, %s,%s)"
+        val = [UserName, Password, FirstName, LastName, Email, "Employee", OrgName]
+        insertUser.execute(sql, val)
+
+        conn.commit()
+
+
+        insertOrg = conn.cursor()
+
+        sql = "INSERT INTO Organizations (OrgName, OwnerUser) VALUES (%s, %s)"
+        val = [OrgName, UserName]
+        insertOrg.execute(sql, val)
+
+        conn.commit()
+        
+        data = {
+            "Registered": "True"
+        }
+
+    #json object response
+    return jsonify(data)
