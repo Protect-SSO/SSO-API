e70311b9091d607738a666adab1c3fa00677081b
davidkenesaw <davidkennesaw@gmail.com>
2023-10-07 15:37:52 -0400

    deleted redundant unused code

--- Server/main.py
+++ Server/main.py
@@ -8,13 +8,7 @@ load_dotenv()#loading env variables
 app = Flask(__name__)
 app.register_blueprint(Auth,url_prefix='/Auth')#blueprint for auth routes
 
-#pymysql config
-conn = pymysql.connect( 
-        host=os.getenv('HOSTDB'), 
-        user=os.getenv('USERDB'),  
-        password = os.getenv('PASSDB'), 
-        db=os.getenv('DBDB'), 
-        ) 
+
 
 #home route used for testing
 @app.route("/")
